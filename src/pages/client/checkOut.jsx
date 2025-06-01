5072715fbd416b45d6e084e0106e7ba63af01746
Nihal <nihalranathunge@gmail.com>
2025-06-01 08:53:59 +0530

    order api working complete

--- src/pages/client/checkOut.jsx
+++ src/pages/client/checkOut.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { BiMinus, BiPlus, BiTrash } from "react-icons/bi";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 
 export default function CheckOutPage() {
 
     const location = useLocation();
     const [cart, setCart] = useState(location.state?.cart ||[]);
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [address, setAddress] = useState("");
 
 
     function getItemsTotal() {
@@ -49,16 +53,55 @@ export default function CheckOutPage() {
     }
 
     function changeQty(index, qty){
-        const newQty = cart[index].qty + qty
-        if(newQty <= 0){
-            newQty = 1
-            return
-        }else{
-            const newCart = [...cart]
-            newCart[index].qty = newQty
-            setCart(newCart)
+        let newQty = cart[index].qty + qty;
+        if (newQty <= 0) {
+            newQty = 1;
+        }
+        const newCart = [...cart];
+        newCart[index].qty = newQty;
+        setCart(newCart);
+    }
+
+
+    async function placeOrder(){
+        const token = localStorage.getItem("token");
+        if (!token) {
+            toast.error("Please login first");
+            return;
+        }
+
+        const orderInformation = {
+            products: [],
+            phone: phoneNumber,
+            // name: "John Doe",
+            address: address
+        };
+
+        for (let i = 0; i < cart.length; i++) {
+            orderInformation.products[i] = {
+                productId: cart[i].productId,
+                quantity: cart[i].qty
+            };
+        }
+
+        try {
+            const res = await axios.post(
+                import.meta.env.VITE_BACKEND_URL + "/api/order",
+                orderInformation,
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            );
+            toast.success("Order placed successfully!");
+        } catch (err) {
+            console.error(err?.response?.data || err.message);
+            toast.error("Error placing order");
         }
     }
+
+
 
     return (
         <div className="bg-gray-200 w-full h-full flex justify-between items-center pl-25 pr-25 pt-4 pb-4">
@@ -70,7 +113,7 @@ export default function CheckOutPage() {
                         (item, index)=>{
                             return(
                                 
-                                <div key={item.productId} className="w-[100%] h-[115px] bg-white my-1 rounded-md flex justify-between items-center p-2">
+                                <div key={item.productId || index} className="w-[100%] h-[115px] bg-white my-1 rounded-md flex justify-between items-center p-2">
                                     {/* Product Image */}
                                     <div className="w-[15%] h-full ml-4">
                                         <img src={item.image[0]} className="w-[100px] h-[100px] object-cover rounded-md" />
@@ -102,7 +145,7 @@ export default function CheckOutPage() {
                                     {/* Actions */}
                                     <div className="w-[100px] h-[100px] flex flex-col justify-top items-end space-y-4">
                                         {/* <button className="text-red-600 cusor-pointer hover:bg-red-600 hover:text-white rounded-full p-2 right-[-35px]"></button> */}
-                                        <button className="text-2xl cusor-pointer hover:bg-red-600 hover:text-white rounded-full"
+                                        <button className="text-2xl cursor-pointer hover:bg-red-600 hover:text-white rounded-full"
                                             onClick={() => {
                                                 removeFromCart(index)
                                             }}><BiTrash/>
@@ -169,8 +212,28 @@ export default function CheckOutPage() {
                         Rs.{getEstimatedTotal().toFixed(2)}
                     </p>
                 </div>    
+
+                {/* Input Shipping Address */}
+                <input 
+                    className="w-full text-sm rounded-md border-2 border-gray-200 py-2 px-4 mt-8" 
+                    type="text" 
+                    placeholder="Phone Number" 
+                    value={phoneNumber} 
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+                <input 
+                    className="w-full text-sm rounded-md border-2 border-gray-200 py-2 px-4 mt-4" 
+                    type="text" 
+                    placeholder="Address" 
+                    value={address} 
+                    onChange={(e) => setAddress(e.target.value)}
+                /> 
+
+
                          
-                <button className="bg-green-600 hover:bg-green-700 cursor-pointer text-white font-semibold py-2 px-4 rounded-md mt-8 hover:bg-green-700 active:bg-green-800">
+                <button className="bg-green-600 hover:bg-green-700 cursor-pointer text-white font-semibold py-2 px-4 rounded-md mt-8 hover:bg-green-700 active:bg-green-800"
+                    disabled={cart.length == 0}
+                    onClick={placeOrder}>
                     Place Order
                 </button>
 
